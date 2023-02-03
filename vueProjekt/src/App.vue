fd24ba6869b51be69269bbbe5b9a74b9cb499357
Wasyl6 <wiktor.78@wp.pl>
2023-02-03 20:23:16 +0100

    Initial commit

---
+++ vueProjekt/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <input type="text" placeholder="todo" v-model="newTask">
+    <button @click="">Dodaj</button>
+    <div v-for="task in tasks" v-bind:key="task.id">
+      <ol>
+      {{ item.title }}
+      </ol>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      newTask: null,
+      tasks: [
+        {title: "Napisać program", completed: false, id:"1"},
+        {title: "Zrobić kawe", completed: false, id:"2"}
+      ]
+    }
+  },
+  methods: {
+    newTask(){
+
+    }
+  }
+}
+</script>
