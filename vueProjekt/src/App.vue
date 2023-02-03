cb05f9e84b95d041b304a3ad1303ee910ca1f75d
Wasyl6 <wiktor.78@wp.pl>
2023-02-03 23:53:59 +0100

    Update App.vue

--- vueProjekt/src/App.vue
+++ vueProjekt/src/App.vue
@@ -3,10 +3,10 @@
     <input type="text" placeholder="Do zrobienia" v-model="newTask">
     <button @click="addTask">Dodaj</button>
     <ul>
-    <li class="task" v-bind:class="{completed: task.completed}" v-for="task in tasks" v-bind:key="task.id">
+    <li class="task" v-bind:class="{finished: task.bool}" v-for="task in tasks" v-bind:key="task.id">
       <ol>
         {{ task.text }}
-        <button @click="">Zrobione</button>
+        <button v-if="!task.bool" @click="removeTask(task.id)">Zrobione</button>
       </ol>
     </li>
     </ul>
@@ -19,7 +19,7 @@ export default {
       newTask: "",
       tasks: [
         {text: "Napisać program", bool: false, id: 1},
-        {text: "Zrobić kawe", bool: false, id: 2}
+        {text: "Zrobić kawe", bool: true, id: 2}
       ]
     }
   },
@@ -31,18 +31,23 @@ export default {
         id: Math.random()
       })
       this.newTask = ""
+    },
+    removeTask(id) {
+      const index = this.tasks.findIndex(el => el.id === id)
+      this.tasks[index].bool = true
     }
   }
 }
 </script>
 <style>
 .task {
+  color: #f75e5e;
   border: 1px solid #550000;
   margin: 8px;
   padding: 10px;
 }
-.completed {
+.finished {
   color: green;
-
+  text-decoration: line-through;
 }
 </style>
