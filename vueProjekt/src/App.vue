e91ae3c2fc3a84b963a9bc0f379c9b3356e36106
Wasyl6 <wiktor.78@wp.pl>
2023-02-03 22:38:39 +0100

    Update App.vue

--- vueProjekt/src/App.vue
+++ vueProjekt/src/App.vue
@@ -3,9 +3,10 @@
     <input type="text" placeholder="Do zrobienia" v-model="newTask">
     <button @click="addTask">Dodaj</button>
     <ul>
-    <li class="task" v-for="task in tasks" v-bind:key="task.id">
+    <li class="task" v-bind:class="{completed: task.completed}" v-for="task in tasks" v-bind:key="task.id">
       <ol>
         {{ task.text }}
+        <button @click="">Zrobione</button>
       </ol>
     </li>
     </ul>
@@ -35,5 +36,13 @@ export default {
 }
 </script>
 <style>
+.task {
+  border: 1px solid #550000;
+  margin: 8px;
+  padding: 10px;
+}
+.completed {
+  color: green;
 
+}
 </style>
