6df08ed5e08171a3395b606a086db51742849f81
Wasyl6 <wiktor.78@wp.pl>
2023-02-04 14:44:23 +0100

    Update App.vue

--- vueProjekt/src/App.vue
+++ vueProjekt/src/App.vue
@@ -1,48 +1,55 @@
 <template>
   <div id="app">
     <div>
-    <h1 class="koniec" v-if="allTasksFinished">Wszystko skończone!</h1>
-    <h2>Lista TODO:</h2>
-    <input type="text" placeholder="Do zrobienia" v-model="new_task">
-    <button @click="addTask">Dodaj</button>
+      <h1 class="koniec" v-if="allTasksFinished">Wszystko skończone!</h1>
+      <h2>Lista TODO:</h2>
+      <input type="text" placeholder="Do zrobienia" v-model="newTaskText">
+      <button @click="addTask">Dodaj</button>
     </div><br>
     <ul>
-    <li class="task" v-bind:class="{finished: task.finished}" v-for="task in tasks" v-bind:key="task.id">
-      <ol>
-        {{ task.text }}
-        <input type="checkbox" v-if="!task.finished" @click="removeTask(task.id)">
-      </ol>
-    </li>
+      <li class="task" v-bind:class="{ finished: task.finished }" v-for="task in tasks" v-bind:key="task.id">
+        <span v-if="editingTaskId !== task.id || task.finished">{{ task.text }}</span>
+        <input type="text" v-model="task.text" v-if="editingTaskId === task.id && !task.finished"/>
+        <button v-if="!task.finished" @click="editingTaskId === task.id ? stopEditingTask() : editTask(task.id)">{{ editingTaskId === task.id ? 'Zatwierdz' : 'Edytuj' }}</button>
+        <input type="checkbox" v-if="!task.finished" @click="completeTask(task.id)">
+      </li>
     </ul>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      new_task: "",
-      tasks: []
+      newTaskText: "",
+      tasks: [],
+      editingTaskId: null,
     }
   },
   methods: {
     addTask() {
       this.tasks.push({
-        text: this.new_task,
+        text: this.newTaskText,
         finished: false,
-        id: Math.random()
+        id: Math.random(),
       })
-      this.new_task = ""
+      this.newTaskText = ""
     },
-    removeTask(id) {
-      const index = this.tasks.findIndex(el => el.id === id)
+    completeTask(id) {
+      const index = this.tasks.findIndex(task => task.id === id)
       this.tasks[index].finished = true
-    }
+    },
+    editTask(id) {
+      this.editingTaskId = id
+    },
+    stopEditingTask() {
+      this.editingTaskId = null
+    },
   },
   computed: {
     allTasksFinished() {
-      return this.tasks.length > 0 && this.tasks.every(task => task.finished==true)
-  }
-  }
+      return this.tasks.length > 0 && this.tasks.every(task => task.finished)
+    },
+  },
 }
 </script>
-//style css w main.css
